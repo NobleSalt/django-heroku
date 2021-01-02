380bc4d5a2074fddc70725862f22360d09546726
noblesalt <izuchukwue2@gmail.com>
2021-01-02 03:09:56 +0100

    main

--- apps/Upload/urls.py
+++ apps/Upload/urls.py
@@ -4,5 +4,5 @@ from apps.Upload.views import UploadFile,ListFile
 app_name = 'upload'
 urlpatterns = [
     path("upload/", UploadFile.as_view(), name="home"),
-    path("files/", ListFile.as_view(), name="files")
+    path("/", ListFile.as_view(), name="files")
 ]
